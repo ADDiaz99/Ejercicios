c288b406e854debc8d3d7497e43bab3e63dee395
ADDiaz99 <113253264+ADDiaz99@users.noreply.github.com>
2023-01-24 18:06:49 -0500

    Ejercicios de Practica 101

---
+++ Condicionales/05_Condicionales.py
@@ -0,0 +1,15 @@
+# Ejercicio 5 
+# Para tributar un determinado impuesto se debe ser mayor de 16 años y 
+# tener unos ingresos iguales o superiores a 1000 € mensuales. 
+# Escribir un programa que pregunte al usuario su edad y sus ingresos mensuales y muestre 
+# por pantalla si el usuario tiene que tributar o no.
+
+#SOLUCION:
+
+edad = int(input('Cual es tu edad? => '))
+ingresos = int(input('Cuales son tus ingresos => '))
+
+if edad > 16 and ingresos >= 1000: 
+    print('Tienes que tributar $$$')
+else:
+    print('No es necesario que tributes.')
