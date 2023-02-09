bbbc11a8b1d5d0a17dd958f94b2b01e890affb49
ADDiaz99 <113253264+ADDiaz99@users.noreply.github.com>
2023-02-08 22:07:32 -0500

    Ejercicios de Practica 101

---
+++ Cadenas/tiktokchallenge.py
@@ -0,0 +1,11 @@
+from random import *
+
+novio = input('Cual es el nombre del novio? =>')
+novia = input('Cual es el nombre de la novia? =>')
+
+comp = randint(1, 100)
+
+
+print(f'{novio} y {novia} son {comp}% compatibles!')
+
+
