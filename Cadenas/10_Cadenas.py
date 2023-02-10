74610394dbb3935e1aa0edcf95f6d3e1def2f4af
ADDiaz99 <113253264+ADDiaz99@users.noreply.github.com>
2023-02-09 20:37:30 -0500

    Ejercicios de Practica 101

---
+++ Cadenas/10_Cadenas.py
@@ -0,0 +1,12 @@
+#Escribir un programa que pregunte por consola por los productos de una cesta de la compra,
+# separados por comas, y muestre por pantalla cada uno de los productos en una línea distinta.
+
+#SOLUCION:
+
+prod = input('Escribe los productos separados por comas =>')
+
+print(prod.replace(',', '\n'))
+
+
+
+
