353ef222bc40ad7e5adf83c6ef36011ff0701284
ADDiaz99 <113253264+ADDiaz99@users.noreply.github.com>
2023-02-21 22:10:02 -0500

    Ejercicios de Practica 101

--- Bucles/09_Bucles.py
+++ Bucles/09_Bucles.py
@@ -1,1 +1,11 @@
-#
+#Escribir un programa que almacene la cadena de caracteres contraseña en una variable, 
+# pregunte al usuario por la contraseña hasta que introduzca la contraseña correcta.
+
+#Solucion:
+
+key = "contraseña"
+password =""
+while password != key:
+    password = input("Introduce la contraseña: ")
+print("Contraseña correcta")
+
