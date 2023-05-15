e088c656bf3440636f45039bdd41d13d9a5d32dc
ADDiaz99 <113253264+ADDiaz99@users.noreply.github.com>
2023-05-14 20:19:47 -0500

    added funciones

---
+++ Funciones/02_Funciones.py
@@ -0,0 +1,8 @@
+#Escribir una función a la que se le pase una cadena <nombre> y muestre por pantalla el saludo ¡hola <nombre>!.
+
+#Solucion
+
+def saludo(nombre):
+    return print(f'Hola {nombre}!')
+
+saludo('Nicky')
