7e5ae562d0f7512571e8bc8d4411c0c98d2403d3
ADDiaz99 <113253264+ADDiaz99@users.noreply.github.com>
2023-05-21 00:07:51 -0500

    added funciones 6

---
+++ Funciones/06_Funciones.py
@@ -0,0 +1,10 @@
+#Escribir una función que reciba una muestra de números en una lista y devuelva su media.
+
+#Solucion:
+
+numeros = [20, 54, 65, 10, 1, 12, 65, 32, 2, 41]
+
+def media(numeros):
+    return print(sum(numeros)/ len(numeros))    
+
+media(numeros)
