a7a19a806a0e4225908fcaffd3e27279482c8dc8
daletski77 <daletski77@gmail.com>
2023-08-29 14:29:47 -0500

    added new exercises

---
+++ Practica/printwidthandheight.py
@@ -0,0 +1,23 @@
+# Auto-generated code below aims at helping you parse
+# the standard input according to the problem statement.
+"""
+Example of output:
+(2,2)=
+OO
+OO
+-----------------
+(4,1)=
+O
+O
+O
+O
+"""
+height = 20
+width = 20
+
+def printzero(height, width):
+    
+    for i in range(height):
+        print("O" * width)
+
+printzero(height, width)
